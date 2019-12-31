1a8d77bda53d8c63927a9f4c89026f79a13ac1b0
Ranika Williams <rnkprice07@gmail.com>
2019-12-31 23:16:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  resources :attractions
+  resources :rides
+  resources :users, only: [:new, :create, :show]
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/session', to: 'session#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
